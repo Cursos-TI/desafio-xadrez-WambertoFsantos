92c83a79f2d322714625e666cb176d4bc34a2849
WambertoFsantos <wambertofsantos@gmail.com>
2025-03-26 18:54:53 +0000

    XadrezNovato

--- xadrez.c
+++ xadrez.c
@@ -10,13 +10,19 @@ int main() {
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    for (int i = 0; i < 5; i++) {
+        printf("Direita e para Cima\n");
+    }
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    for (int i = 0; i < 5; i++) {
+        printf("Cima\n");
+    }
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    for (int i = 0; i < 8; i++) {
+        printf("Direita e para Cima\n");
+    }
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
